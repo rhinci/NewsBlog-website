06a12ea8e480eb5a5a6b4205c2f6d25bab912765
rhinci <dashalupanova@gmail.com>
2025-10-25 09:11:32 +1000

    login, register, logout views

--- blog/views.py
+++ blog/views.py
@@ -90,3 +90,34 @@ def create_article(request):
         form = ArticleForm()
     
     return render(request, 'blog/create_article.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'blog/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'blog/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
