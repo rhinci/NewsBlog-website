7dfef5d61154a797d5b3bf7f9ef8bccb21d47e2d
rhinci <dashalupanova@gmail.com>
2025-10-27 23:43:18 +1000

    edit_article.html and url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('edit-article/<int:id>/', views.edit_article, name='edit_article'),
 ]
