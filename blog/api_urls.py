410aa4c0c27191333cc8865ff1da9652e483d3dc
rhinci <dashalupanova@gmail.com>
2025-11-03 00:00:58 +1000

    jwt urls

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api import ArticleApi, CommentApi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 router = DefaultRouter()
 router.register(r'articles', ArticleApi)
@@ -8,4 +13,7 @@ router.register(r'comment', CommentApi)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
